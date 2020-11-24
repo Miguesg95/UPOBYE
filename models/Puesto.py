a8f8babc2499a3b8c74f6bd916a23da34fd94fa3
aescfue <adrianescoz@protonmail.com>
2020-11-24 22:50:53 +0100

    update

--- models/Puesto.py
+++ models/Puesto.py
@@ -4,9 +4,10 @@ from odoo import models, fields, api
 
 
 class Puesto(models.Model):
-    _name = 'upobye.Puesto'
+    _name = 'upobye.puesto'
     _description = 'Distintos Puestos'
     
-    ID = fields.Char(string="Nombre", required=True, help="Nombre")
+    name = fields.Char(string="Nombre", size=60, help="Nombre puesto")
+    ID = fields.Char(string="ID", required=True, help="ID")
     required = fields.Boolean("¿Es necesario?")
     qualification = fields.Text()
