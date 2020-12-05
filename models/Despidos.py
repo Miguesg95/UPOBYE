a7ad6172ee46711a148c9c9f5d66b6734999f35c
aescfue <adrianescoz@protonmail.com>
2020-12-05 19:29:45 +0100

    Revert "Merge branch 'main' of https://github.com/Miguesg95/UPOBYE into main"
    This reverts commit 7eae38fa5e05d554e7c9d7d9cbe0febbadf5a0a3, reversing
    changes made to c9ad93be57b91e7e825438d1787359e998aae1f0.

--- models/Despidos.py
+++ models/Despidos.py
@@ -4,14 +4,15 @@ from odoo import models, fields, api
 
 
 class Despidos(models.Model):
-     _name = 'upobye.despidos'     #nombremodulo.nombreclase
-     _description = 'upobye.despidos'
+     _name = 'upobye.despidos' 
+     #nombremodulo.nombreclase
+    _description = 'upobye.despidos'
 
-     name = fields.Integer(string="Finiquito",required=True,help="Indemnización")
-     motivo = fields.Text()
-     start = fields.Datetime('Starts', required=True, autodate =True)
-     end = fields.Datetime('Ends', required=True, autodate =True)
-     motivo_despido = fields.Selection([('jubilacion','Jubilación'),
+     name = fields.Integer(string="finiquito",required=True,help="Indemnización")
+    motivo = fields.Text()
+    start = fields.Datetime('Starts', required=True, autodate =True)
+    end = fields.Datetime('Ends', required=True, autodate =True)
+    motivoDespido = fields.Selection([('jubilacion','Jubilación'),
                                     ('fin de contrato','Fin de contrato'),
                                     ('baja indefinida','Baja indefinida')
                                     ],'Motivo del despido')
@@ -22,4 +23,5 @@ class Despidos(models.Model):
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
 
