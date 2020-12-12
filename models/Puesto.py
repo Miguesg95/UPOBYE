4712ec858641ebdfcec4c65b817842958fcaa693
aescfue <adrianescoz@protonmail.com>
2020-12-12 15:19:49 +0100

    boton

--- models/Puesto.py
+++ models/Puesto.py
@@ -6,10 +6,14 @@ from odoo import models, fields, api
 class Puesto (models.Model):
     _name = 'upobye.puesto'
     _description = 'Distintos Puestos'
-    
-    name = fields.Char(string="Nombre",required=True, help="Nombre puesto")
-    ID = fields.Char(string="ID", required=True, help="ID")
-    required = fields.Boolean(string="Necesario", help="¿Es necesario o no?")
-    qualification = fields.Text(string="Requisitos", required=True, help="requisitos del puesto")
+
+    name = fields.Char(string="Nombre", required=True, size=60, help="Nombre puesto")
+    ID = fields.Char(string="ID", required=True, size=40, help="ID")
+    required = fields.Boolean(string="Necesario", help="¿Es un puesto que debe estar siempre ocupado?")
+    qualification = fields.Text(string="Requisitos", size=300, help="requisitos del puesto")
 
     contrato_id = fields.One2many('upobye.contrato', 'puesto_id', 'Contrato')
+    
+    def btn_limpiarContratos(self):
+        self.write({'contrato_id':[(5,)]})
+    
--- models/TipoContrato.py
+++ models/TipoContrato.py
@@ -6,19 +6,19 @@ from odoo import models, fields, api
 class TipoContrato(models.Model):
     _name = 'upobye.tipo_contrato'
     _description = 'upobye Tipo de Contrato'
-    
-    name = fields.Char(string="Nombre", size=60, help="Nombre Tipo Contrato")
-    tipo = fields.Selection([('indOrdinario','indefinido ordinario'),
-                            ('indIncentivado',' indefinido incentivado'),
-                            ('fijoDiscontinuo','fijo discontinuo'),
-                            ('contObra','Contrato de obra'),
-                            ('contEventual','Contrato eventual'),
-                            ('contInterinidad','Contrato de interinidad'),
-                            ('contTempInc','Contrato temporal incentivado'),
-                            ('contRelevo','Contrato de relevo'),
-                            ('contFormacion','Contrato para la formación y el aprendizaje'),
-                            ('contPracticas','Contrato en practicas'),
-                                    ],'Tipo de contrato')
-    description = fields.Text(string="Descripcion del contrato", required=True, help="breve descripcion")
-    contrato_id = fields.One2many('upobye.contrato', 'tipo_contrato_id', 'Contrato')
 
+    name = fields.Char(string="Nombre", size=60, help="Nombre Tipo Contrato")
+    tipo = fields.Selection([('indOrdinario', 'indefinido ordinario'),
+                             ('indIncentivado', ' indefinido incentivado'),
+                             ('fijoDiscontinuo', 'fijo discontinuo'),
+                             ('contObra', 'Contrato de obra'),
+                             ('contEventual', 'Contrato eventual'),
+                             ('contInterinidad', 'Contrato de interinidad'),
+                             ('contTempInc', 'Contrato temporal incentivado'),
+                             ('contRelevo', 'Contrato de relevo'),
+                             ('contFormacion',
+                              'Contrato para la formación y el aprendizaje'),
+                             ('contPracticas', 'Contrato en practicas'),
+                             ], 'Tipo de contrato', required=True)
+    description = fields.Text(string="Descripcion del contrato", size=200, help="breve descripcion")
+    contrato_id = fields.One2many('upobye.contrato', 'tipo_contrato_id', 'Contrato')
