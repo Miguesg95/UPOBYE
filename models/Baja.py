7aa4343695d756968982daf572ce2dddfe97360e
Miguel Angel <masglyl4@gmail.com>
2020-12-13 12:15:06 +0100

    baja con contrains

--- models/Baja.py
+++ models/Baja.py
@@ -11,13 +11,13 @@ class Baja (models.Model):
     name = fields.Char(string="Nombre del empleado",required=True, size=20, help="Nombre del empleado que solicita la baja")
     duracionDias = fields.Integer(string='Duración en días',required=True, help="Tiempo en días que dura la baja", default=0)
     fechaInicio = fields.Datetime(string="Fecha de inicio", required=True)
-    fechaFin = fields.Datetime(string="Fecha de finalización")
+    fechaFin = fields.Datetime(string="Fecha de finalización", readonly=True)
     motivoBaja = fields.Text(string="Motivo", required=True, size=200, help="Motivo por el que se concedió la baja")
 
     contrato_id = fields.One2many('upobye.contrato', 'baja_id', 'Contrato')
 
-    @api.constrains('fechaInicio','fechaFin','duracionDias')
+    @api.constrains('fechaInicio')
     def _fechas_baja(self):
-            added_fecha = self.fechaInicio 
-            added_fecha.day = self.fechaFin.day + self.duracionDias
-            self.write({'fechaFin' : added_fecha})
+        self.write({'fechaFin' : self.fechaInicio + timedelta(days=self.duracionDias)})
+
+            
