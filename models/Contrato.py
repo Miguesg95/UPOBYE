10cfef22b6448ac5f6b11cfddd165529724abd90
Miguel Angel <masglyl4@gmail.com>
2020-12-21 17:54:50 +0100

    otro push

--- models/Contrato.py
+++ models/Contrato.py
@@ -20,7 +20,7 @@ class Contrato(models.Model):
     tipo_contrato_id = fields.Many2one('upobye.tipo_contrato', string="Tipo del contrato")
     puesto_id = fields.Many2one('upobye.puesto', string="Puesto")
     proyecto_id = fields.Many2one('upobye.proyecto', string='Proyecto')
-    empleado_id = fields.One2many('upobye.empleado', 'contrato_id', 'Empleado')
+    #empleado_id = fields.One2many('upobye.empleado', 'contrato_id', 'Empleado')
 
     @api.onchange('sueldo')
     def onchange_contrato(self):
