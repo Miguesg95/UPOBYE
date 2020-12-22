039b1f28b9dc3fad5d0e1a398a8d654e1340bc8f
Miguel Angel <masglyl4@gmail.com>
2020-12-22 22:29:38 +0100

    d

--- models/Empleado.py
+++ models/Empleado.py
@@ -6,7 +6,6 @@ from odoo import models, fields, api
 class Empleado(models.Model):
     _name = 'upobye.empleado'
     _description = 'Empleados'
-    #_inherit = ['hr.employee']
 
-    contrato_id = fields.Many2one('upobye.contrato', string="Contratos")
+    contrato_id = fields.Many2one('upobye.contrato', 'empleado_id', 'Contratos')
     
