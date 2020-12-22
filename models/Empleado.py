e943c1c120ea2e4a3ad940a3125860d4e9761e3a
mtoromateos <mtoromateos98@gmail.com>
2020-12-22 22:50:44 +0100

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit e6d05b2c02828f70c0356d4686ac9790d17fdf64.

--- models/Empleado.py
+++ models/Empleado.py
@@ -6,6 +6,7 @@ from odoo import models, fields, api
 class Empleado(models.Model):
     _name = 'upobye.empleado'
     _description = 'Empleados'
+    #_inherit = ['hr.employee']
 
-    contrato_id = fields.Many2one('upobye.contrato', 'empleado_id', 'Contratos')
+    contrato_id = fields.Many2one('upobye.contrato', string="Contratos")
     
